37a7bc52c17cdb56422fa0b9cf3f3ac3d67acb29
dirie93 <mdirie19@gmail.com>
2023-04-20 21:50:57 +0100

    added uppercase, lowercase, symbols and integers to character set

--- Develop/script.js
+++ Develop/script.js
@@ -11,5 +11,69 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword() {
+  var password = "";
+  var passwordLength = 8;
+  var integers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbols = ["!", "@", "?", ":", "£", "#", ";", "%", "~"];
+  // adding lowercase letters
+  var lowerCase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var upperCase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
